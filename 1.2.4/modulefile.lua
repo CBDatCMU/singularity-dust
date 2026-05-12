3071c2cbf10fd699a52c35830abbe63323c8ca9d
Ivan E. Cao-Berg <icaoberg@gmail.com>
2026-05-11 21:48:12 -0400

    Updated modulefile.lua

--- 1.2.4/modulefile.lua
+++ 1.2.4/modulefile.lua
@@ -1,41 +1,46 @@
 --
 -- dust 1.2.4 modulefile
 --
--- "URL: https://www.psc.edu/resources/software"
--- "Category: Other"
--- "Description: du + rust = dust. Like du but more intuitive."
--- "Keywords: singularity utilities"
 
 whatis("Name: dust")
 whatis("Version: 1.2.4")
-whatis("Category: Other")
-whatis("URL: https://www.psc.edu/resources/software")
+whatis("Category: Utilities")
+whatis("URL: https://github.com/bootandy/dust")
 whatis("Description: du + rust = dust. Like du but more intuitive.")
+whatis("Keywords: singularity utilities disk")
+
+family("dust")
 
 help([[
 du + rust = dust. Like du but more intuitive.
+Runs inside a Singularity container.
 
 To load the module type
 
-> module load dust/1.2.4
+  > module load dust/1.2.4
 
 To unload the module type
 
-> module unload dust/1.2.4
+  > module unload dust/1.2.4
 
 Documentation
 -------------
 For help, type
 
-> dust --help
+  > dust --help
 
-Repository
-----------
-Tools included in this module are
+Upstream repository: https://github.com/bootandy/dust
+
+Tools
+-----
 * dust
 ]])
 
-local package = "dust"
-local version = "1.2.4"
-local base    = pathJoin("/containers",package,version)
+local package  = "dust"
+local version  = "1.2.4"
+local root     = os.getenv("CONTAINERS_ROOT") or "/containers"
+local base     = pathJoin(root, package, version)
+
+conflict("dust")
 prepend_path("PATH", base)
+setenv("DUST_SIF", pathJoin(base, "singularity-" .. package .. "-" .. version .. ".sif"))
